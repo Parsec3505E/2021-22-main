bbb644ff2a385828728f24e90302eacc93c38106
freezedcheese <58059756+freezedcheese@users.noreply.github.com>
2021-08-23 22:19:36 -0400

    main PROS file creation

---
+++ Parsec-2021-22-main/src/main.cpp
@@ -0,0 +1,17 @@
+#include "main.h"
+
+void initialize() {
+	pros::lcd::initialize();
+}
+
+void disabled() {}
+
+void competition_initialize() {}
+
+void autonomous() {
+
+}
+
+void opcontrol() {
+
+}
