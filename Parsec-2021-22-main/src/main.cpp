207a35e82d1f977da90c005064a044490f0d54a2
Ar-Vi <aryavishnu06@gmail.com>
2021-08-24 17:44:45 -0400

    Drivetrain class creation

--- Parsec-2021-22-main/src/main.cpp
+++ Parsec-2021-22-main/src/main.cpp
@@ -1,5 +1,9 @@
 #include "main.h"
 
+class drivetrain {
+	
+}
+
 void initialize() {
 	pros::lcd::initialize();
 }
@@ -11,10 +15,11 @@ void competition_initialize() {}
 void autonomous() {}
 
 void opcontrol() {
+	pros::Controller master (pros::E_CONTROLLER_MASTER);
+
 	pros::Motor spinny(2);
-	spinny.move(127);
 
-	pros::delay(1000);
-
-	spinny.move(0);
+	while (true){
+		spinny.move(master.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+	}
 }
