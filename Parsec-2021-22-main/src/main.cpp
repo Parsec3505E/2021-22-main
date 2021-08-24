2e7d93e998c9234035e9c313c76811cf861b314e
Ar-Vi <aryavishnu06@gmail.com>
2021-08-24 16:59:28 -0400

    Fix motor spin test

--- Parsec-2021-22-main/src/main.cpp
+++ Parsec-2021-22-main/src/main.cpp
@@ -2,20 +2,19 @@
 
 void initialize() {
 	pros::lcd::initialize();
-
 }
 
 void disabled() {}
 
 void competition_initialize() {}
 
-void autonomous() {
-
-}
+void autonomous() {}
 
 void opcontrol() {
-	pros::Motor spinny(1);
+	pros::Motor spinny(2);
 	spinny.move(127);
 
+	pros::delay(1000);
 
+	spinny.move(0);
 }
